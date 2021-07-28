9010cd71cfcf0e90c9a18fd6f1a7b78bc2ff140d
GökayyŞ <gokay.shn@gmail.com>
2021-07-28 21:15:06 +0300

    first commit

---
+++ javascriptclock/js/clock.js
@@ -0,0 +1,45 @@
+let name = prompt("Adınız nedir?");
+document.querySelector("#myName").innerHTML = name;
+ 
+ function showTime() {
+   var date = new Date();
+   var h = date.getHours();
+   var m = date.getMinutes();
+   var s = date.getSeconds();
+   var session = date.getDay();
+   switch (session) {
+    case (1) :
+        session="Pazartesi";
+        break;
+    case (2) :
+        session = "Salı";
+        break;
+    case (3) :
+        session="Çarşamba";
+        break;
+    case (4) :
+        session = "Perşembe";
+        break;
+    case (5) :
+        session = "Cuma";
+        break;
+    case (6) :
+        session = "Cumartesi";
+        break;
+    case (7) :
+        session = "Pazar";
+        break;
+   }
+   h = h < 10 ? "0" + h : h;
+   m = m < 10 ? "0" + m : m;
+   s = s < 10 ? "0" + s : s;
+
+   var time = h + ":" + m + ":" + s + " " + session;
+   document.querySelector('#myClock').innerHTML = time;
+  
+ 
+   setTimeout(showTime, 1000);
+ }
+ showTime();
+  
+
